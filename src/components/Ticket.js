e65b53c076fcf8fc00868f963f72f0fea98a980d
Matt Stroud <lixingke06@gmail.com>
2020-06-27 23:12:01 -0700

    add proptypes

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,8 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Ticket(props) {
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <h3>{props.location} - {props.names}</h3>
@@ -10,6 +9,12 @@ function Ticket(props) {
       <hr />
     </React.Fragment>
   );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string
 }
 
 export default Ticket;
